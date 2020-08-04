ea1eed446b0626cd913bd687a9216cf9763f35fc
Danilo Pinheiro <dan.augusto@gmail.com>
2020-08-04 18:56:16 -0300

    Destructuring arguments

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
-//Statelss Funcition Component
-const NavBar = (props) => {
+//Statelss Function Component
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <span className="navbar-brand mb-0 h1">
         Navbar
         <span className="badge badge-pill badge-secondary m-2">
-          {props.totalCounters}
+          {totalCounters}
         </span>
       </span>
     </nav>
